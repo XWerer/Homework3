da38f4457b937916e322831c003281418b2a3df7
spudteo <37929393+spudteo@users.noreply.github.com>
2018-04-18 14:14:22 +0200

    iniziowerrgwergfrg

--- src/main/java/it/unipd/dei/bdc1718/G08HM3.java
+++ src/main/java/it/unipd/dei/bdc1718/G08HM3.java
@@ -18,7 +18,7 @@ public class G08HM3 {
       throw new IllegalArgumentException("Expecting the file name on the command line");
     }
 
-    // Setup Spark
+    // Setup Spark efwefwe
     SparkConf conf = new SparkConf(true).setAppName("Preliminaries");
     JavaSparkContext sc = new JavaSparkContext(conf);
 
