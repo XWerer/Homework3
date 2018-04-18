1a19470ca669ac1cecdd48eb8c14f018a2613977
XWerer <dadomw2@gmail.com>
2018-04-18 13:59:51 +0200

    prova commit cazzooo

--- src/main/java/it/unipd/dei/bdc1718/G08HM3.java
+++ src/main/java/it/unipd/dei/bdc1718/G08HM3.java
@@ -18,6 +18,8 @@ public class G08HM3 {
       throw new IllegalArgumentException("Expecting the file name on the command line");
     }
 
+    //prova commento e commit
+
     // Setup Spark
     SparkConf conf = new SparkConf(true).setAppName("Preliminaries");
     JavaSparkContext sc = new JavaSparkContext(conf);
