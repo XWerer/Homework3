d2ce8791b88b881135c5732b8911f6d3a9ceb227
spudteo <37929393+spudteo@users.noreply.github.com>
2018-04-18 18:05:43 +0200

    Ho fatto il primo che trova i k-center, non ho misurato il tempo però. Sicuramente c'è un modo più elegante di farlo, però funzia usando due for uno su k e su P, quindi dovrebbe essere O(P*k) come vuole lui. Solo che ho usato la funzione che diceva il prof sqdist, e sul first further traversal penso voglia la sola distanza, non il quadrato.

--- src/main/java/it/unipd/dei/bdc1718/G08HM3.java
+++ src/main/java/it/unipd/dei/bdc1718/G08HM3.java
@@ -13,17 +13,68 @@ import java.util.Scanner;
 
 public class G08HM3 {
 
-  public static void main(String[] args) throws Exception {
-    if (args.length == 0){
-      throw new IllegalArgumentException("Expecting the file name on the command line");
-    }
+    public static void main(String[] args) throws Exception {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Expecting the file name on the command line");
+        }
 
-    gerherhuerbhreburebhu
 
-    // Setup Spark efwefwe
-    SparkConf conf = new SparkConf(true).setAppName("Preliminaries");
-    JavaSparkContext sc = new JavaSparkContext(conf);
+        // Setup Spark
+        SparkConf conf = new SparkConf(true).setAppName("Preliminaries");
+        JavaSparkContext sc = new JavaSparkContext(conf);
 
-    ArrayList<Vector> point = InputOutput.readVectorsSeq(args[0]);
+        ArrayList<Vector> point = InputOutput.readVectorsSeq(args[0]);
+
+        //tempo del k-center
+
+         ArrayList<Vector> centri = kcenter(point,10);
+
+         //just for debug
+         System.out.println("i centri sono:"+centri.size());
+         for(int i =0;i < centri.size();i++) {
+             System.out.println(centri.get(i));
+         }
+
+
+  }
+  public static ArrayList<Vector> kcenter (ArrayList<Vector> P ,int k){ //O( |P| * k )
+
+    ArrayList<Vector> centers = new ArrayList<Vector> ();
+    Vector max= Vectors.zeros(1);  //mi salvo il quello che sta a distanza massima
+    ArrayList<Double> dist = new ArrayList<Double> (); //distanza massima
+    double distanza=0.0;
+    double temp=0.0;
+    boolean primo = true;
+    //così so quale cancellare dall'array list delle distanze
+    int indice=0;
+    //nelle slide c'è scritto prende un punto arbitrario, prendo il primo
+    centers.add(P.get(0));
+    //lo tolgo così non lo considero nel calcolo delle distanze
+    P.remove(0);
+    for(int i=0; i<k-1;i++ ){//k-1 perchè il primo lo do io
+      for(int j=0;j<P.size();j++){
+          if(primo) {//primo giro devo calcolare la distanza dal primo punto
+              dist.add(Vectors.sqdist(centers.get(0), P.get(j)));
+              if(dist.get(j)> distanza) { //trovo il punto più distante da il resto dal punto inizale
+                max = P.get(j);
+                distanza = dist.get(j);
+                indice=j;
+          }
+        }else{//negli altri
+              temp=dist.get(j) + Vectors.sqdist(centers.get(i), P.get(j));
+              dist.set(j,temp);
+              if(dist.get(j)> distanza) { //trovo il punto più distante da il resto dei punti
+                  max = P.get(j);
+                  distanza = dist.get(j);
+                  indice=j;
+              }
+          }
+      }//fine for P
+        primo = false;
+        centers.add(max);                                        //quando lho trovato lo aggiungo a quelli trovati e lo
+        P.remove(max);                                           //tolgo così non lo conisidero più
+        dist.remove(indice);                                     //tolgo anche la distanza annessa
+    }//fine for k
+    return centers;
   }
 }
